266f60d622fb97ce1bf16a47794960c0acb900ac
benjamin kakai <benjaminkakai001@gmail.com>
2024-01-28 13:40:17 +0300

    Add files via upload

---
+++ UserService.java
@@ -0,0 +1,29 @@
+package com.example.lmsbackend.service;
+
+import com.example.lmsbackend.model.User;
+import com.example.lmsbackend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User createUser(String username, String rawPassword) {
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(passwordEncoder.encode(rawPassword));
+        return userRepository.save(user);
+    }
+
+    // Additional methods for user management can be added here
+}
